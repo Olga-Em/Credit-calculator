5555a89af761fa1b3ebf44e9a8e179242f363c5f
Olga Em <olga.em020@gmail.com>
2022-02-10 23:17:15 +0700

    Added files

---
+++ src/CreditPaymentService.java
@@ -0,0 +1,16 @@
+public class CreditPaymentService {
+    public int calculate (int creditTerm, int amount){
+        double payment;
+        int months = creditTerm * 12;
+        double p = 9.99/100/months;
+        double b = 1+p;
+        PowService pow2 = new PowService();
+        double powMonth = pow2.pow (b,months);
+
+            payment = amount * (p+(p/(powMonth-1)));
+
+        int creditPayment = (int) payment;
+
+        return creditPayment;
+    }
+}
