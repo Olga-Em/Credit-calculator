c6898807400a029955d4d07efc496e54c917364a
Olga Em <olga.em020@gmail.com>
2022-03-20 13:33:19 +0700

    Adjustment 2

--- src/CreditPaymentService.java
+++ src/CreditPaymentService.java
@@ -1,10 +1,10 @@
 public class CreditPaymentService {
     public double calculate(int creditTerm, int amount) {
-       // double payment;
+       
         int months = creditTerm * 12;
         double monthlyInterestRate = 9.99 / 100 / 12;
         double a = 1 + monthlyInterestRate;
-        double creditPayment = amount * (monthlyInterestRate + (monthlyInterestRate / (Math.pow( a, months) - 1)));
+        double creditPayment = amount * (monthlyInterestRate + (monthlyInterestRate / (Math.pow(a, months) - 1)));
         return creditPayment;
     }
 }
